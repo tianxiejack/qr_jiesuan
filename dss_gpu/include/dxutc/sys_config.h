429ff342c285616bd2a9e20e4506a0f1fedaaa00
xx <xx>
2017-07-24 14:50:11 +0800

    fix compile error

--- dss_gpu/include/dxutc/sys_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef _SYSTEM_CONFIGURE_H
-#define _SYSTEM_CONFIGURE_H
-
-Int32 syscfg_load(char *configfile, Int32 *data, Int32 dataSize);
-Int32 syscfg_save(char *configfile, Int32 *data, Int32 dataSize);
-Int32 syscfg_load_block(char *configfile, Int32 *data, Int32 blkId);
-
-#endif
--- dss_gpu/include/malloc_align.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MALLOC_ALIGN_H
-#define _MALLOC_ALIGN_H
-
-void* MallocAlign( int nSize );
-
-void FreeAlign(void *p);
-
-#endif
-
