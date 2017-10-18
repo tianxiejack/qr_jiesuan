29a625671c636e8cff209f1559e77418043442a4
xx <xx@com>
2017-10-18 18:08:28 +0800

    修改eAnglePosn字符显示

--- dss_gpu/src/PRI/osdProcess.cpp
+++ dss_gpu/src/PRI/osdProcess.cpp
@@ -410,9 +410,9 @@ OSDText_Obj g_Text[OSD_TEXT_SIZE]=
 
 	{eAnglePosn,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	45+LOFFSET,		500,	0,	{0}},
 	{eAngleDipX,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	45+LOFFSET,		465,	0,	{0}},
-	{eAngleDipY,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	170+LOFFSET,		465,	0,	{0}},
-	{eAngleMach,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	170+LOFFSET,		500,	0,	{0}},
-	{eAngleGred,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	285+LOFFSET,		500,	0,	{0}},
+	{eAngleDipY,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	200+LOFFSET,		465,	0,	{0}},
+	{eAngleMach,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	200+LOFFSET,		500,	0,	{0}},
+	{eAngleGred,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	330+LOFFSET,		500,	0,	{0}},
 	{eGunTip,				eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	240+LOFFSET,		70,	0,	{0}},
 	{eLaserjlx,	eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	420,		500,	0,	{0}},
 	{eLaserjly,	eOsd_Hide,	eOsd_Update,	eWhite,	eTransparent,	MAX_CONTEXT_LEN,	530,		500,	0,	{0}},
@@ -2302,19 +2302,19 @@ int ls2tmp = 0000;
 			sprintf(pStr,"%c%c%c%c:      %s",201,144,145,146,Posd[eLaserState]);//������ĩѡͨ
 			break;
 		case eAnglePosn:
-			sprintf(pStr,"%c%c%+5.2f",190,239,getTurretTheta()); //��λ��
+			sprintf(pStr,"%c%c%+07.2f",190,239,getTurretTheta()); //��λ��
 			break;
 		case eAngleDipX:
-			sprintf(pStr,"%cX:%+5.2f",220,getPlatformPositionX()); //���X
+			sprintf(pStr,"%cX:%+06.2f",220,getPlatformPositionX()); //���X
 			break;
 		case eAngleDipY:
-			sprintf(pStr,"%cY:%+5.2f",220,getPlatformPositionY()); //���Y
+			sprintf(pStr,"%cY:%+06.2f",220,getPlatformPositionY()); //���Y
 			break;
 		case eAngleMach:
-			sprintf(pStr,"%cY:%+5.2f",200,getMachGunAngle()); //��ǹY
+			sprintf(pStr,"%cY:%+06.2f",200,getMachGunAngle()); //��ǹY
 			break;
 		case eAngleGred:
-			sprintf(pStr,"%cY:%+5.2f",211,getGrenadeAngle()); //��Y
+			sprintf(pStr,"%cY:%+06.2f",211,getGrenadeAngle()); //��Y
 			break;
 		case eGunTip:
 			sprintf(pStr,"%s",Posd[eGunTip]); //����ȷ����ʾ
